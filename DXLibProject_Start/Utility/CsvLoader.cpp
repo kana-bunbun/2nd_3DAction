9cc3f81f5f5d36642c1f8515cfb4a4ba71013d3b
kana-bunbun <gaomuke186@gmail.com>
2026-05-31 05:59:23 +0900

    csv読み込みのクラスでの無駄な処理の消去

--- DXLibProject_Start/Utility/CsvLoader.cpp
+++ DXLibProject_Start/Utility/CsvLoader.cpp
@@ -6,8 +6,6 @@
 #include<fstream>
 #include<cassert>
 
-#include<typeinfo>
-
 namespace {
 	const char* const kResourePath = "Resource\\CSV\\";
 }
@@ -66,29 +64,14 @@ bool CsvLoader::LoadCSVData(std::string filePath)
 		
 		// 抜き出した1行をさらに1文字ずつ区切る
 		auto split = Split(buf, ',');
-		int size = split.size();
-
-		// 1番最後の行の列のサイズを設定する
-		m_loadData[m_loadData.size()-1].resize(size);
-	}
-	 ifs= std::ifstream(filePath.c_str());
-
-	// 1行ずつ読み込み
-	for (int y = 0; y < m_loadData.size(); y++) {
-		
-		// 1行に何も値がなければスキップ
-		if (!std::getline(ifs, buf)) continue;
-		
-		// 1文字ずつ区切る
-		auto split = Split(buf, ',');
-		
 		// 列の数だけ繰り返す
 		for (int x = 0; x < split.size(); x++) {
-		
+
 			// 値を代入する
-			m_loadData[y][x] = split[x];
+			m_loadData[m_loadData.size() - 1].push_back(split[x]);
 		}
 	}
+
 	// ファイルを閉じる
 	ifs.close();
 
