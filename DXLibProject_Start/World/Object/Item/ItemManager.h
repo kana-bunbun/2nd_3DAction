3c18de5d71686781d1a1a59615280eb9774d34ea
kana-bunbun <gaomuke186@gmail.com>
2026-07-10 07:24:50 +0900

    カメラビューを取得

--- DXLibProject_Start/World/Object/Item/ItemManager.h
+++ DXLibProject_Start/World/Object/Item/ItemManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<memory>
+#include"../../../Camera/Camera.h"
 class ItemObjectManager;
 class ItemManager
 {
@@ -15,9 +16,12 @@ public:
 
 public:
 	ItemCursor* GetItemCursor() { return m_pItemCursor.get(); }
+	void SetCameraView(const Camera::CameraView& view) { m_view = view; }
+	const Camera::CameraView& GetCameraView() { return m_view; }
 private:
 	std::unique_ptr<ItemObjectManager>m_pItemObjectManager;
 	// アイテムカーソル
 	std::unique_ptr<ItemCursor> m_pItemCursor;
+	Camera::CameraView m_view;
 };
 
